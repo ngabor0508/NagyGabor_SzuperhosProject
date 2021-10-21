d701ba789c08879b8b04427e1b4eb1d48a09c624
Nagy Gabor <ngabor0508@gmail.com>
2021-10-21 13:31:51 +0200

    Beolvasas kesz.

--- src/com/nagy/gabor/Kepregeny.java
+++ src/com/nagy/gabor/Kepregeny.java
@@ -1,7 +1,19 @@
 package com.nagy.gabor;
 
-public class Kepregeny {
-    public static void szereplok(String faljnev){
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Kepregeny {
+    public static ArrayList<Szuperhos> szuperhosLista = new ArrayList<>();
+
+    public static void szereplok(String faljnev) throws FileNotFoundException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader(faljnev));
+        String egysor = br.readLine();
+        while (egysor != null){
+            String [] szo = egysor.split(" ");
+        }
     }
 }
