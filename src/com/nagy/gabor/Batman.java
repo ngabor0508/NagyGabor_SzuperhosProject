346154ea34a1c68e94e2430ceaca6d74882f07a0
Nagy Gabor <ngabor0508@gmail.com>
2021-10-21 13:08:40 +0200

    Implementáslás kész a Batman osztályba

--- src/com/nagy/gabor/Batman.java
+++ src/com/nagy/gabor/Batman.java
@@ -1,4 +1,19 @@
 package com.nagy.gabor;
 
-public class Batman {
+public class Batman implements Szuperhos, Milliardos{
+
+    @Override
+    public void kutyutKeszit() {
+
+    }
+
+    @Override
+    public boolean legyzoziE(Szuperhos s) {
+        return false;
+    }
+
+    @Override
+    public double mekkoraAzEreje() {
+        return 0;
+    }
 }
